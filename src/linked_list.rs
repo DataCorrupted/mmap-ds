120c9aeef9c8df36eefc9e07b033326a8e2fa6ff
Peter <PeterRong96@gmail.com>
2020-05-20 22:04:26 -0700

    create linked list done
    Signed-off-by: Peter <PeterRong96@gmail.com>

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,12 +1,29 @@
-use std::marker::{Sized, PhantomData};
-use std::path::PathBuf;
+use nix::{
+    fcntl::OFlag,
+    sys::mman::{mmap, munmap, MapFlags, ProtFlags},
+};
+use std::marker::{PhantomData, Sized};
+use std::{
+    fs::{File, OpenOptions},
+    mem,
+    os::unix::io::AsRawFd,
+    path::PathBuf,
+    ptr::{self, null_mut},
+};
 
+type FileItemIdx = usize;
+type ListItemIdx = usize;
+
+struct LinkedListMetaData {
+    size: FileItemIdx,
+    file_len: FileItemIdx,
+    list_len: ListItemIdx,
+    head: Option<ListItemIdx>,
+}
 struct LinkedList<T: Sized> {
-    size: usize,
-    len: usize,
-    fd: usize,
-    head: Option<usize>,
-    phantom: PhantomData<T>,
+    ptr: *mut u8,
+    metadata: *mut LinkedListMetaData,
+    _phantom: PhantomData<T>,
 }
 
 struct Item<T: Sized> {
@@ -15,8 +32,8 @@ struct Item<T: Sized> {
 }
 
 impl<T: Sized> Item<T> {
-    pub fn new(item: T) -> Item<T>{
-        Item{
+    pub fn new(item: T) -> Item<T> {
+        Item {
             item: item,
             next: None,
         }
@@ -24,41 +41,74 @@ impl<T: Sized> Item<T> {
 }
 
 impl<T: Sized> LinkedList<T> {
-    pub fn new(path: PathBuf) -> LinkedList<T> {
+    pub fn empty_with_size(
+        path: PathBuf,
+        size: FileItemIdx,
+    ) -> Result<LinkedList<T>, failure::Error> {
+        let metadata = LinkedListMetaData {
+            size: size,
+            file_len: 0,
+            list_len: 0,
+            head: None,
+        };
+        let file = OpenOptions::new().write(true).read(true).open(path)?;
+        let size = mem::size_of::<LinkedListMetaData>() + size * mem::size_of::<T>();
+        let ptr = unsafe {
+            mmap(
+                ptr::null_mut(),
+                size as usize,
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_PRIVATE,
+                file.as_raw_fd(),
+                0,
+            )?
+        } as *mut u8;
+        let metadata_ptr = ptr as *mut LinkedListMetaData;
+        unsafe {
+            *metadata_ptr = metadata;
+        }
+        Ok(Self {
+            ptr: unsafe { ptr.add(mem::size_of::<LinkedListMetaData>()) },
+            metadata: metadata_ptr,
+            _phantom: PhantomData,
+        })
+    }
+
+    pub fn from_file(path: PathBuf) -> Result<LinkedList<T>, failure::Error> {
         unimplemented!()
     }
-    pub fn resize(&self, size: usize){
+    pub fn resize(&self, size: FileItemIdx) {
         unimplemented!()
     }
-    pub fn insert_after(&self, item: T, pos: usize) {
+    pub fn insert_after(&self, item: T, pos: ListItemIdx) {
         unimplemented!();
     }
-    pub fn remove(pos: usize) {
+    pub fn remove(pos: ListItemIdx) {
         unimplemented!();
     }
-    pub fn nth_file(idx: usize) -> &'static Item<T> {
+    pub fn nth_file(idx: FileItemIdx) -> &'static Item<T> {
         unimplemented!()
     }
-    pub fn nth_mut_file(idx: usize) -> &'static mut Item<T> {
+    pub fn nth_mut_file(idx: FileItemIdx) -> &'static mut Item<T> {
         unimplemented!()
     }
-    pub fn nth_list(idx: usize) -> &'static Item<T> {
+    pub fn nth_list(idx: ListItemIdx) -> &'static Item<T> {
         unimplemented!()
     }
-    pub fn nth_mut_list(idx: usize) -> &'static mut Item<T> {
+    pub fn nth_mut_list(idx: ListItemIdx) -> &'static mut Item<T> {
         unimplemented!()
     }
 }
 
-impl<T: Sized> std::ops::Index<usize> for LinkedList<T> {
+impl<T: Sized> std::ops::Index<ListItemIdx> for LinkedList<T> {
     type Output = Item<T>;
-    fn index(&self, index: usize) -> &Self::Output {
+    fn index(&self, index: ListItemIdx) -> &Self::Output {
         unimplemented!()
     }
 }
 
-impl<T: Sized> std::ops::IndexMut<usize> for LinkedList<T> {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+impl<T: Sized> std::ops::IndexMut<ListItemIdx> for LinkedList<T> {
+    fn index_mut(&mut self, index: ListItemIdx) -> &mut Self::Output {
         unimplemented!()
     }
 }
